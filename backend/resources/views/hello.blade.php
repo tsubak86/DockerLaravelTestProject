64e269ce8a945642d7ccde4b72447a9fbb2f3fe7
tsubak86 <tsubak86@gmail.com>
2021-12-26 15:51:19 +0900

    変更

--- backend/resources/views/hello.blade.php
+++ backend/resources/views/hello.blade.php
@@ -1,5 +1,15 @@
 <html>
     <body>
         <h1>HELLO Laravel<h1>
+            <h1>DBから表示しているよ<h1>
+            <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+                <br>
+                @foreach ($items as $item)
+                <tr>
+                    <td>{{$item -> name}}</td>
+                    <td>{{$item -> mail}}</td>
+                    <td>{{$item -> age}}</td>
+                </tr>
+            @endforeach
     </body>
 </html>
---
+++ backend/resources/views/hello.blade.php.old
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>HELLO Laravel<h1>
+    </body>
+</html>
