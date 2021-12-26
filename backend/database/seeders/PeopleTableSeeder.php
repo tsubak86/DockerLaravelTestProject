89b3792251d3299161559dcbf4b828577d3b0717
tsubak86 <tsubak86@gmail.com>
2021-12-26 15:31:26 +0900

    更新

---
+++ backend/database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'id' => 1,
+            'name' => 'test',
+            'mail' => 'test',
+            'age' => 20,
+        ];
+    DB::table('people')->insert($param);
+    }
+
+}
